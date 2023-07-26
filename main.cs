f66f83ce84154df837d9d271cd9a2f65c1cdd19a
pedrocarvalhal <61318529+pedrocarvalhal@users.noreply.github.com>
2023-07-26 18:56:45 +0100

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SQLi_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var user = args[0];
+                var pwd = Encrypt(args[1]);
+                Login(user, pwd);
+            }
+            catch  
+            {
+                //12312312312333311155666
+                Console.WriteLine("An error has occurred !!");
+            }
+            
+        }
+
+        private static  string Encrypt(string plain)
+        {
+            return plain;
+        }
+
+        private static void Login(string username,string password)
+        {
+            try
+            {
+                using (var conn = new SqlConnection("conn..."))
+                {
+                    var sql = "SELECT * FROM Users WHERE username = '" + username + "' AND pwd = '" + password + "'";
+                    using (var cmd = new SqlCommand(sql))
+                    {
+                        cmd.Connection = conn;
+                        cmd.ExecuteScalar();
+                    }
+
+                }
+            }
+            catch  
+            {
+
+                Console.WriteLine("An error has occurred !!");
+            }
+           
+        }
+    }
+}
